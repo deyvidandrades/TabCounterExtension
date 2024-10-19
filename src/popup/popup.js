267326ab9054a05c5f76095621d27f444d82112d
deyvidandrades <deyvid-asilva@hotmail.com>
2024-10-19 16:02:21 -0300

    v1.2.3

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,51 +1,52 @@
 chrome.windows.getCurrent(function (window) {
-
-    chrome.storage.sync.get(["recorde"]).then((result) => {
-        carregarListaTabs(window.id, result.recorde ? result.recorde : {"atual": 0, "anterior": 0, "data": ""})
-    });
+  chrome.storage.sync.get(["recorde"]).then((result) => {
+    carregarListaTabs(window.id, result.recorde ? result.recorde : { atual: 0, anterior: 0, data: "" });
+  });
 });
 
 document.getElementById("btn_zerar").addEventListener("click", function () {
-    zerarRecordes()
+  zerarRecordes();
 });
 
+//Localization
+document.getElementById("guias_abertas").innerText = chrome.i18n.getMessage("guias_abertas");
+document.getElementById("guias_janela_atual").innerText = chrome.i18n.getMessage("guias_janela_atual");
+document.getElementById("recorde_atual").innerText = chrome.i18n.getMessage("recorde_atual");
+document.getElementById("resumo").innerText = chrome.i18n.getMessage("resumo");
+document.getElementById("criado_por").innerText = chrome.i18n.getMessage("criado_por");
+
 function carregarListaTabs(idJanelaAtual, recorde) {
-    chrome.tabs.query({}, function (tabs) {
-        let lista_janelas = []
-        let lista_janela_atual = []
+  chrome.tabs.query({}, function (tabs) {
+    let lista_janelas = [];
+    let lista_janela_atual = [];
 
-        tabs.forEach(function (tab) {
-            if (!lista_janelas.includes(tab.windowId))
-                lista_janelas.push(tab.windowId)
+    tabs.forEach(function (tab) {
+      if (!lista_janelas.includes(tab.windowId)) lista_janelas.push(tab.windowId);
 
-            if (tab.windowId === idJanelaAtual)
-                lista_janela_atual.push(tab)
-        });
-
-        updateUI(tabs.length, lista_janela_atual.length, lista_janelas.length, recorde.atual, recorde.anterior, recorde.data)
+      if (tab.windowId === idJanelaAtual) lista_janela_atual.push(tab);
     });
+
+    updateUI(tabs.length, lista_janela_atual.length, lista_janelas.length, recorde.atual, recorde.anterior, recorde.data);
+  });
 }
 
 function updateUI(numTabs, numTabsAtual, numJanelas, recorde, anterior, data) {
-    document.getElementById('guias').innerHTML = numTabs
-    document.getElementById('janelas').innerHTML = `Em ${numJanelas} janelas`
+  document.getElementById("guias").innerHTML = numTabs;
+  document.getElementById("janelas").innerHTML = `${chrome.i18n.getMessage("em")} ${numJanelas} ${chrome.i18n.getMessage("janelas")}`;
 
-    document.getElementById('atual').innerHTML = numTabsAtual
+  document.getElementById("atual").innerHTML = numTabsAtual;
 
-    document.getElementById('recorde').innerHTML = recorde
+  document.getElementById("recorde").innerHTML = recorde;
 
-    if (data !== "" && data !== undefined)
-        document.getElementById('data').innerHTML = `Em ${data}`
+  if (data !== "" && data !== undefined) document.getElementById("data").innerHTML = `${chrome.i18n.getMessage("em")} ${data}`;
 
-    salvarDados(recorde, anterior, data)
+  salvarDados(recorde, anterior, data);
 }
 
 function salvarDados(recorde, anterior, data) {
-    chrome.storage.sync.set({"recorde": {"atual": recorde, "anterior": anterior, "data": data}}).then(() => {
-    });
+  chrome.storage.sync.set({ recorde: { atual: recorde, anterior: anterior, data: data } }).then(() => {});
 }
 
 function zerarRecordes() {
-    chrome.storage.sync.set({"recorde": {"atual": 0, "anterior": 0, "data": ""}}).then(() => {
-    });
+  chrome.storage.sync.set({ recorde: { atual: 0, anterior: 0, data: "" } }).then(() => {});
 }
--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,41 +1,39 @@
 chrome.runtime.onInstalled.addListener(() => {
-    //salvarDados(87, 82)
-    getNumTabs()
+  //salvarDados(87, 82)
+  getNumTabs();
 });
 
 chrome.tabs.onCreated.addListener((tab) => {
-    getNumTabs()
+  getNumTabs();
 });
 chrome.tabs.onRemoved.addListener((tab) => {
-    getNumTabs()
+  getNumTabs();
 });
 
 function getNumTabs() {
-    chrome.tabs.query({}, function (tabs) {
-        chrome.action.setBadgeText({text: `${tabs.length}`});
-        chrome.action.setBadgeBackgroundColor({color: '#A9DED8'});
+  chrome.tabs.query({}, function (tabs) {
+    chrome.action.setBadgeText({ text: `${tabs.length}` });
+    chrome.action.setBadgeBackgroundColor({ color: "#72cfd6" });
 
-        chrome.storage.sync.get(["recorde"]).then((result) => {
-            let recorde = result.recorde
-            if (recorde) {
-                if (tabs.length > recorde.anterior) {
-                    salvarDados(tabs.length, recorde.atual, formatarData())
-                }
-            }
-        });
-
+    chrome.storage.sync.get(["recorde"]).then((result) => {
+      let recorde = result.recorde;
+      if (recorde) {
+        if (tabs.length > recorde.anterior) {
+          salvarDados(tabs.length, recorde.atual, formatarData());
+        }
+      }
     });
+  });
 }
 
 function salvarDados(recorde, anterior, data) {
-    chrome.storage.sync.set({"recorde": {"atual": recorde, "anterior": anterior, "data": data}}).then(() => {
-    });
+  chrome.storage.sync.set({ recorde: { atual: recorde, anterior: anterior, data: data } }).then(() => {});
 }
 
 function formatarData(date = new Date()) {
-    const dia = date.toLocaleString('default', {day: '2-digit'})
-    const mes = date.toLocaleString('default', {month: '2-digit'})
-    const ano = date.toLocaleString('default', {year: 'numeric'})
+  const dia = date.toLocaleString("default", { day: "2-digit" });
+  const mes = date.toLocaleString("default", { month: "2-digit" });
+  const ano = date.toLocaleString("default", { year: "numeric" });
 
-    return [dia, mes, ano].join('/');
+  return [dia, mes, ano].join("/");
 }
